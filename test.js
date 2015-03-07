bb0c987df56deee649823bd7fb139511d8296f45
Rob Catch <rcatchpole@appcelerator.com>
2015-03-07 20:05:14 +0000

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-var l = require('logger.js');
-console.log("Log message");
-console.error("Error message");
-console.warn("Warning message");
-console.info("Info message");
-console.debug("Debug message");
